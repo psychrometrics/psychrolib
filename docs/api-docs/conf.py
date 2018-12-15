0784b933a015a30f3096848eb83cfda1ff5f4031
dmey <dmey@users.noreply.github.com>
2018-12-14 20:58:27 -0500

    update api docs

--- docs/api-docs/conf.py
+++ docs/api-docs/conf.py
@@ -81,11 +81,62 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
 html_theme_options = {
-    # Disable showing
-    #  the sidebar. Defaults to 'false'
-    'nosidebar': True,
+    # Navigation bar title. (Default: ``project`` value)
+    'navbar_title': "PsychroLib",
+
+    # A list of tuples containing pages or urls to link to.
+    # Valid tuples should be in the following forms:
+    #    (name, page)                 # a link to a page
+    #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
+    #    (name, "http://example.com", True) # arbitrary absolute url
+    # Note the "1" or "True" value above as the third argument to indicate
+    # an arbitrary url.
+    # 'navbar_links': [
+    #     ("Examples", "examples"),
+    #     ("Link", "http://example.com", True),
+    # ],
+
+    # Global TOC depth for "site" navbar tab. (Default: 1)
+    # Switching to -1 shows all levels.
+    # 'globaltoc_depth': -1
+
+    # Include hidden TOCs in Site navbar?
+    #
+    # Note: If this is "false", you cannot have mixed ``:hidden:`` and
+    # non-hidden ``toctree`` directives in the same page, or else the build
+    # will break.
+    #
+    # Values: "true" (default) or "false"
+    'globaltoc_includehidden': "true",
+
+    # HTML navbar class (Default: "navbar") to attach to <div> element.
+    # For black navbar, do "navbar navbar-inverse"
+    'navbar_class': "navbar",
+
+    # Fix navigation bar to top of page?
+    # Values: "true" (default) or "false"
+    'navbar_fixed_top': "true",
+
+    # Location of link to source.
+    # Options are "nav" (default), "footer" or anything else to exclude.
+    'source_link_position': "",
+
+    # Bootswatch (http://bootswatch.com/) theme.
+    #
+    # Options are nothing (default) or the name of a valid theme such
+    # such as "cosmo" or "sandstone".
+    #
+    # Example themes:
+    # * flatly
+    # * sandstone (v3 only)
+    # * united
+    # * yeti (v3 only)
+    #'bootswatch_theme': "flatly",
+
+    # Choose Bootstrap version.
+    # Values: "3" (default) or "2" (in quotes)
+    'bootstrap_version': "3",
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/api-docs/index.rst
+++ docs/api-docs/index.rst
@@ -1,5 +1,15 @@
-PsychroLib
-=================
+=============================
+ PsychroLib API Documentation
+=============================
+
+The page provides a comprehensive description of all the 
+functions available in PsychroLib. Although it describes 
+the Python version of the library, PsychroLib uses a 
+common API across all the supported language implementations.
+Please see the `PsychroLib repository on GitHub`_ if you are looking to
+download or find out more about PsychroLib.
+
+.. _`PsychroLib repository on GitHub`: https://github.com/psychrometrics/psychrolib
 
 .. automodule:: psychrolib
    :members:
