91dd95fc4f4f964aaeaf472666edd9a92da5c0c7
dmey <dmey@users.noreply.github.com>
2019-03-19 21:58:30 +0000

    Revert "use corrective term in SI formulae for GetSatVapPres to erase the gap at 0 C (#26)" (#29)
    This reverts commit 223ef028370c13f8e9d55dfeed43fcfb290b9f45.
    This issue will be handled by a new PR addressing the discontinuity in the NR method in GetTDewPointFromVapPres.

--- src/c/psychrolib.c
+++ src/c/psychrolib.c
@@ -647,16 +647,11 @@ double GetHumRatioFromEnthalpyAndTDryBulb  // (o) Humidity ratio in lb_H₂O lb_
 
 // Return saturation vapor pressure given dry-bulb temperature.
 // Reference: ASHRAE Handbook - Fundamentals (2017) ch. 1 eqn. 5 & 6
-// Notes: the SI formulae show a discontinuity at 0 C. In rare cases this discontinuity creates issues
-// in GetTDewPointFromVapPres. To avoid the problem, a small corrective term is added/subtracted
-// to the ASHRAE formulae to make the formulae continuous at 0 C. The effect on the results is
-// negligible (0.005%), well below the accuracy of the formulae
 double GetSatVapPres            // (o) Vapor Pressure of saturated air in Psi [IP] or Pa [SI]
   ( double TDryBulb             // (i) Dry bulb temperature in °F [IP] or °C [SI]
   )
 {
   double LnPws, T;
-  double CORRECTIVE_TERM_SI = 4.851e-05; // Small corrective term to make the function continuous at 0 C.
 
   if (isIP())
   {
@@ -679,10 +674,10 @@ double GetSatVapPres            // (o) Vapor Pressure of saturated air in Psi [I
     T = GetTKelvinFromTCelsius(TDryBulb);
     if (TDryBulb >= -100. && TDryBulb <= 0.)
       LnPws = -5.6745359E+03 / T + 6.3925247 - 9.677843E-03 * T + 6.2215701E-07 * T * T
-          + 2.0747825E-09 * pow(T, 3) - 9.484024E-13 * pow(T, 4) + 4.1635019 * log(T) + CORRECTIVE_TERM_SI;
+          + 2.0747825E-09 * pow(T, 3) - 9.484024E-13 * pow(T, 4) + 4.1635019 * log(T);
     else if (TDryBulb > 0. && TDryBulb <= 200.)
       LnPws = -5.8002206E+03 / T + 1.3914993 - 4.8640239E-02 * T + 4.1764768E-05 * T * T
-        - 1.4452093E-08 * pow(T, 3) + 6.5459673 * log(T) - CORRECTIVE_TERM_SI;
+        - 1.4452093E-08 * pow(T, 3) + 6.5459673 * log(T);
     else
       return INVALID;             // TDryBulb is out of range [-100, 200]
   }
