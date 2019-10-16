cbbcbfd6e4d06ffcaa5d8ce94cedb1571865fac4
DJGosnell <DJGosnell@gmail.com>
2019-10-16 13:59:53 -0400

    Relocated all Psychrometric classes into the same psychrolib.cs file. Updated version to 2.2.0.

--- src/c_sharp/PsychroLib/psychrolib.cs
+++ src/c_sharp/PsychroLib/psychrolib.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * PsychroLib (version 2.1.1) (https://github.com/psychrometrics/psychrolib)
- * Copyright (c) 2018 D. Thevenard and D. Meyer for the current library implementation
+ * Copyright (c) 2018 D. Thevenard and D. Meyer, D. Gosnell for the current library implementation
  * Copyright (c) 2017 ASHRAE Handbook — Fundamentals for ASHRAE equations and coefficients
  * Ported to C# by https://github.com/DJGosnell
  * Licensed under the MIT License.
@@ -1076,5 +1076,78 @@ namespace PsychroLib
 
             return value;
         }
+    }
+
+    /// <summary>
+    /// Contains output results of a Psychrometric calculation.
+    /// </summary>
+    public class PsychrometricValue
+    {
+
+        /// <summary>
+        /// Dry bulb temperature in °F [IP] or °C [SI]
+        /// </summary>
+        public double TDryBulb { get; set; }
+
+        /// <summary>
+        /// Wet bulb temperature in °F [IP] or °C [SI]
+        /// </summary>
+        public double TWetBulb { get; set; }
+
+        /// <summary>
+        /// Atmospheric pressure in Psi [IP] or Pa [SI]
+        /// </summary>
+        public double Pressure { get; set; }
+
+        /// <summary>
+        /// Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
+        /// </summary>
+        public double HumRatio { get; set; }
+
+        /// <summary>
+        /// Dew point temperature in °F [IP] or °C [SI]
+        /// </summary>
+        public double TDewPoint { get; set; }
+
+        /// <summary>
+        /// Relative humidity [0-1]
+        /// </summary>
+        public double RelHum { get; set; }
+
+        /// <summary>
+        /// Partial pressure of water vapor in moist air in Psi [IP] or Pa [SI]
+        /// </summary>
+        public double VapPres { get; set; }
+
+        /// <summary>
+        /// Moist air enthalpy in Btu lb⁻¹ [IP] or J kg⁻¹ [SI]
+        /// </summary>
+        public double MoistAirEnthalpy { get; set; }
+
+        /// <summary>
+        /// Specific volume ft³ lb⁻¹ [IP] or in m³ kg⁻¹ [SI]
+        /// </summary>
+        public double MoistAirVolume { get; set; }
+
+        /// <summary>
+        /// Degree of saturation [unitless]
+        /// </summary>
+        public double DegreeOfSaturation { get; set; }
+    }
+
+    /// <summary>
+    /// Standard unit systems
+    /// </summary>
+    public enum UnitSystem
+    {
+        /// <summary>
+        /// Imperial Units
+        /// </summary>
+        IP = 1,
+
+        /// <summary>
+        /// Metric System Units
+        /// </summary>
+        SI = 2
     }
 }
