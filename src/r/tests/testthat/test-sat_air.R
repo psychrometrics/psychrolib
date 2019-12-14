f22299a431d535c751d6e1764dbb210d612b0489
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-12-15 01:18:45 +0800

    Reorganize tests for rel_hum.R.

--- src/r/tests/testthat/test-sat_air.R
+++ src/r/tests/testthat/test-sat_air.R
@@ -14,22 +14,6 @@ test_that("saturated vapor pressure calculations match ASHRAE's tabulated result
     expect_equal_rel(GetSatVapPres(300.0), 67.0206, rel = 0.0003)
 })
 
-# Test that the NR in GetTDewPointFromVapPres converges.
-# This test was known problem in versions of PsychroLib <= 2.0.0
-test_that("the NR in GetTDewPointFromVapPres converges", {
-    SetUnitSystem("IP")
-    TDryBulb <- seq(-148, 392, 1)
-    RelHum <- seq(0, 1, 0.1)
-    Pressure <- seq(8.6, 17.4, 1)
-    expect_silent(
-        for (RH in RelHum) {
-            for (P in Pressure) {
-                GetTWetBulbFromRelHum(TDryBulb, RH, P)
-            }
-        }
-    )
-})
-
 # Test saturation vapour pressure calculation
 # The values are tested against the values published in Table 3 of ch. 1 of the 2017 ASHRAE Handbook - Fundamentals
 # over the range [-100, +200] C
